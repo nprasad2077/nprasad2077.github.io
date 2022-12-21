0fa69795a57d777c97e83ef8d689126f4f40d71a
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2022-12-21 14:59:17 -0600

    created components blank

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>About: include downloadable PDF link to resume.
+
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>Projects: links to github page/live project</div>
+  )
+}
+
+export default Projects
