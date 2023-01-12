09def7ad151eeea21f306a2f3b40de5fd3973e15
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-01-12 13:13:21 -0600

    welcome div

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useState } from 'react'
 
 // Components
-import Sidebar from '../Sidebar/Sidebar'
 import About from '../About/About'
 import Projects from '../Projects/Projects'
 import Resume from '../Resume/Resume'
@@ -15,23 +14,12 @@ const Home = () => {
 
 
   return (
-    <div
-      class={`flex items-center justify-center bg-gray-300 flex-row ${
-        darkToggle && 'dark'
-      }`}
-    >
-      <label class="toggleDarkBtn">
-        <input type="checkbox" onClick={() => setDarkToggle(!darkToggle)} />
-        <span class="slideBtnTg round"></span>
-      </label>
-      <div class='flex-col'>
+    <div>
+      <div>
         <NavHeader />
-        <div class='mt-20'>
+      </div>
+      <div class=''>
         <Welcome />
-        <About />
-        <Projects />
-        <Resume />
-        </div>
       </div>
     </div>
   )
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Card } from 'flowbite-react'
 
 const Welcome = () => {
   return (
-    <div class='mt-10'>
-    <a href="#" class="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://static.vecteezy.com/system/resources/thumbnails/002/099/712/small_2x/mountain-beautiful-landscape-background-design-illustration-free-vector.jpg" alt=""/>
-        <div class="flex flex-col justify-between p-4 leading-normal">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Welcome</h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        </div>
-    </a>
+    <div className='welcome'>
+      <Card>
+        <img src='src/assets/pics/mountains.jpg'></img>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Nikhil Prasad
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Full Stack Web Developer
+        </p>
+      </Card>
     </div>
   )
 }
