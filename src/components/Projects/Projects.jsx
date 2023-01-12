60e91280e83ef40ebb02efa99efe47f94c6967a7
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-01-11 19:57:27 -0600

    new projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Card } from 'flowbite-react'
 
 const Projects = () => {
   return (
@@ -11,6 +12,22 @@ const Projects = () => {
         </div>
     </a></div>
 <div class='flex-col'>
+<div className="max-w-sm">
+  <Card
+    horizontal={true}
+    imgSrc="src\assets\shot\planetarium.png"
+  >
+    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      Planetarium
+    </h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400">
+    Planetarium is an application used to display information about the planets in our Solar System.
+    </p>
+    <p className="font-normal text-gray-700 dark:text-gray-400">
+    Planetarium is an application used to display information about the planets in our Solar System.
+    </p>
+  </Card>
+</div>
 <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
         <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
@@ -19,7 +36,7 @@ const Projects = () => {
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Planetarium</h5>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Planetarium is an application used to display information about the planets in our Solar System.</p>
         <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
             <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
