e764e899575b78ba66d597f1ddcac4edf254a5cf
Nikhil Prasad <114438461+nprasad2077@users.noreply.github.com>
2023-01-11 20:47:33 -0600

    projects updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -47,51 +47,36 @@ const Projects = () => {
 <div className="max-w-sm">
   <Card
     horizontal={false}
-    imgSrc="src\assets\shot\travel.png"
+    imgSrc="src\assets\shot\nutri.png"
   >
     <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
       Nutri-Scan
     </h5>
     <p className="font-normal text-gray-700 dark:text-gray-400">
-    Travel Base is an application used to view and create memories about travel destinations.
+    Nutri-Scan is an application for researching nutritional and environmental facts about food products. It can quickly display helpful nutritional summaries and interpret package information.
     </p>
     <p className="font-normal text-gray-700 dark:text-gray-400">
-    Travel Base is deployed on a React.js front-end that uses data from a Node.js/MongoDB/Mongoose back-end.
+    Nutri-Scan is a React.js front-end application that calls to the blank.com server for data.
+    </p>
+  </Card>
+</div>
+<div className="max-w-sm">
+  <Card
+    horizontal={false}
+    imgSrc="src\assets\shot\pokemon.png"
+  >
+    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      Pokemon Match!
+    </h5>
+    <p className="font-normal text-gray-700 dark:text-gray-400">
+      Pokemon Match! is a fun memory match game. Players are to find a matching pokemon until all cards are turned over.
+    </p>
+    <p className="font-normal text-gray-700 dark:text-gray-400">
+      Pokemon Match! uses vanilla HTML/CSS/JavaScript to handle visuals and game logic.
     </p>
   </Card>
 </div>
 
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </a>
-    <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nutri-Scan</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-        </a>
-    </div>
-</div>
-
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-    </a>
-    <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Pokemon Match!</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-            <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-        </a>
-    </div>
-</div>
 </div>
 
     </div>
