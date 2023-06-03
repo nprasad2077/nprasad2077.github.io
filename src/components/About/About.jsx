f3665e35db35c3ff3095cccaf801fbced8fcd803
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-06-02 22:52:41 -0500

    name revert

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
       <div className="about p-2">
         <Card>
           <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mb-5">
-            Nick Prasad
+            Nikhil Prasad
           </h5>
           <p className="font-normal text-gray-700 dark:text-gray-400 mb-10">
             I am a software developer based in Houston, TX with experience in
